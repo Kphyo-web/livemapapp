6ad352f8d2d29a11b155cefcba166d1a0a29468c
Kphyo-web <kophyo2107520@gmail.com>
2025-06-17 01:52:04 +0630

    updated index.html and server.js

--- server.js
+++ server.js
@@ -4,24 +4,27 @@ const socketIo = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
-const io = socketIo(server,{ cors:{ origin:'*'}})
+const io = socketIo(server,{ cors:{ origin:'*' }});
 
-// Serve static files if you have a "public" directory
-app.use(express.static('public'));
+// Serve files from root directory
+app.use(express.static('.'));
+
+const locations = {};
 
 io.on('connection',(socket)=>{
   console.log('Client connected')
 
   socket.on('location',(data)=>{
-    // Broadcast this location to all other clients
-    socket.broadcast.emit('location',data);
+    locations[data.userId] = { lat: data.lat, lng: data.lng };
+    io.emit('locations', locations);
   });
 
   socket.on('disconnect',()=>{
-    console.log('Client disconnected');
+    console.log('Client disconnected')
+    // You could delete their location here if you wish
   })
 });
 
-// Listen on port (Glitch typically uses process.env.PORT)
+// Listen on port
 const port = process.env.PORT || 3000;
 server.listen(port,()=> console.log(`Server listening on port ${port}`))
